2c1ea2a8178b1484eaa7f8e7af84884b419e4f79
Boris <boris.vicena@gmail.com>
2024-09-01 21:10:58 +0200

    Supabase Auth v2

--- app/index.tsx
+++ app/index.tsx
@@ -1,21 +1,7 @@
 import { supabase } from "@/utils/supabase";
-import { Redirect, router } from "expo-router";
+import { Redirect, router, Slot, Stack } from "expo-router";
 import { useEffect } from "react";
 
 export default function App() {
-  useEffect(() => {
-    supabase.auth.getSession().then(({ data: { session } }) => {
-      if (session) {
-        router.replace("/(tabs)/home");
-      }
-    });
-
-    supabase.auth.onAuthStateChange((_event, session) => {
-      if (session) {
-        router.replace("/(tabs)/home");
-      } else {
-        router.replace("/(auth)/onboarding");
-      }
-    });
-  }, []);
+  return <Redirect href="/(auth)/onboarding" />;
 }
