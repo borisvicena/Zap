622460d5e5671a383420b22cf8ce0861fd684569
Boris <boris.vicena@gmail.com>
2024-09-01 20:24:02 +0200

    Stream Chat Provider

--- app/(app)/_layout.tsx
+++ app/(app)/_layout.tsx
@@ -1,40 +1,12 @@
-import { FontAwesome } from "@expo/vector-icons";
-import { Slot, Stack } from "expo-router";
-import { useEffect } from "react";
-import { StreamChat } from "stream-chat";
-import { OverlayProvider, Chat } from "stream-chat-expo";
-
-const client = StreamChat.getInstance("bkdnyzkuywnp");
+import { Stack } from "expo-router";
+import ChatProvider from "../providers/ChatProvider";
 
 export default function TabsLayout() {
-  useEffect(() => {
-    const connect = async () => {
-      await client.connectUser(
-        {
-          id: "jlahey",
-          name: "Jim Lahey",
-          image: "https://i.imgur.com/fR9Jz14.png",
-        },
-        client.devToken("jlahey")
-      );
-
-      // const channel = client.channel("messaging", "the_park", {
-      //   name: "The Park",
-      // });
-
-      // await channel.watch();
-    };
-
-    connect();
-  });
-
   return (
-    <OverlayProvider>
-      <Chat client={client}>
-        <Stack>
-          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        </Stack>
-      </Chat>
-    </OverlayProvider>
+    <ChatProvider>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      </Stack>
+    </ChatProvider>
   );
 }
